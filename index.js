b21be3b3a02d7e1762bc5cc25086737449d9f795
sumeraali2009 <165699963+sumeraali2009@users.noreply.github.com>
2024-04-22 16:36:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+//2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+//would you like to learn some Python today?
+//let firstName : string ="Eric";
+//console.log (firstName);
+//console.log(`hello ${firstName}, "would you like to learn some Python today"?`)
+//3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.		
+//4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output /should look something like the
+//following, including the quotation marks:
+//let name1 : string = ("Hazrat Muhammad Sallallaho Alehi Wassallam said")
+//console.log(`${name1}"get education from lap to grave"`)
+//5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+//let famous_person: string = "Hazrat Ali"
+//let message : string = " Respect your elders and love your children"
+//console.log (`${famous_person} ${message}`)
+//6. Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each
+//character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces
+//console.log("hellomyworld")
+//console.log("hello\tmy\tworld")
+//console.log("hello\nmy\nworld")
+//7.8   Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// You should create four lines that look like this:
+//_____________________________________________
+//console.log(5 + 3)
+//____________________________________________
+//Your output should simply be four lines with the number 8 appearing once on each line.
+//console.log(5+3)
+//console.log(10-2)
+//console.log(4*2)
+//console.log(16/2)
+//9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favouritenumber = 12;
+console.log("my favouritenumber is ".concat(favouritenumber, "."));
